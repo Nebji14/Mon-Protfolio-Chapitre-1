b37259bb5d4db4ba599690a1bb0f28010e5ed8c6
Nebji14 <bendav192837@gmail.com>
2025-06-23 11:33:03 +0200

    Premiere verion de mon porfolio

--- components/head.php
+++ components/head.php
@@ -70,7 +70,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><strong>Portfolio | DAVID Benjamin</strong></title>
+    <title>Portfolio | DAVID Benjamin</title>
     <meta name="description" content="Portfolio | DAVID Benjamin">
     <meta name="author" content="DAVID Benjamin">
     <meta name="robots" content="">
