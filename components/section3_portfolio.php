17a4e204e1cd9cc13feb30d7889c0d0678547332
Nebji14 <bendav192837@gmail.com>
2025-06-23 09:31:25 +0200

    Premiere version de mon portfolio

--- components/section3_portfolio.php
+++ components/section3_portfolio.php
@@ -5,24 +5,24 @@
             <h2 class="underlin_about">Mes réalisations</h2>
 
             <div class="align-items-start d-flex gap-4">
-                <img src="media/Benjamin.png" alt="" title="">
-                <img src="media/Ben2_.png" alt="" title="">
-                <img src="media/logo2.png" alt="" title="">
-                <img src="media/UI_UX.png" alt="" title="">
+                <img src="media/Benjamin.png" alt="dessin de david benjamin" title="dessin de david benjamin">
+                <img src="media/Ben2_.png" alt="dessin de david benjamin" title="dessin de david benjamin">
+                <img src="media/logo2.png" alt="dessin du logo de david benjamin" title="logo de david benjamin">
+                <img src="media/UI_UX.png" alt="dessin de logo ui ux" title="logo ui ux">
             </div>
 
             <div class="align-items-start d-flex gap-4">
-                <img src="media/others.png" alt="" title="">
-                <img src="media/img_dev.png" alt="" title="">
-                <img src="media/CMS.png" alt="" title="">
-                <img src="media/logo_cal.png" alt="" title="">
+                <img src="media/others.png" alt="dessin du logo autre" title="logo autre">
+                <img src="media/img_dev.png" alt="dessin du logo d'un balise de code" title="logo d'un balise de code">
+                <img src="media/CMS.png" alt="dessin de logo de cms" title="logo de cms">
+                <img src="media/logo_cal.png" alt="dessin de logo de calendrier" title="logo de calendrier">
             </div>
 
             <div class="align-items-start d-flex gap-4">
-                <img src="media/logo_car.png" alt="" title="">
-                <img src="media/Logo_homme.png" alt="" title="">
-                <img src="media/logo_langue_.png" alt="" title="">
-                <img src="media/logo_loc_.png" alt="" title="">
+                <img src="media/logo_car.png" alt="dessin de logo de voiture" title="logo de voiture">
+                <img src="media/Logo_homme.png" alt="dessin de logo de genre masculin" title="logo de genre masculin">
+                <img src="media/logo_langue_.png" alt="dessin de logo langue partiqué" title="logo langue partiqué">
+                <img src="media/logo_loc_.png" alt="dessin de logo d'un balise de localisation" title="logo d'un balise de localisation">
             </div>
 
         </section>
